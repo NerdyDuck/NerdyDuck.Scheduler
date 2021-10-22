4fbd5d950962a98d9e671f808e004582e2fc2f53
Daniel Kopp <dak@nerdyduck.de>
2021-10-22 13:52:54 +0200

    Restructured source and projects

--- src/Scheduler/Scheduler.cs
+++ src/Scheduler/Scheduler.cs
@@ -26,25 +26,18 @@
  ******************************************************************************/
 #endregion
 
-using NerdyDuck.CodedExceptions;
 using NerdyDuck.Collections.Concurrent;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
-using Windows.Foundation;
-using Windows.System.Threading;
 
 namespace NerdyDuck.Scheduler
 {
-	// UWP specific part of implementation
+	// Full framework specific part of implementation
 	partial class Scheduler<T>
 	{
 		#region Private fields
-		private BlockingConcurrentDictionary<ScheduledTask<T>, IAsyncAction> ScheduledTasks;
+		private BlockingConcurrentDictionary<ScheduledTask<T>, CancellationTokenSchedulerAction> ScheduledTasks;
 		#endregion
 
 		#region Private methods
@@ -53,7 +46,7 @@ namespace NerdyDuck.Scheduler
 		/// </summary>
 		private void Init()
 		{
-			ScheduledTasks = new BlockingConcurrentDictionary<ScheduledTask<T>, IAsyncAction>();
+			ScheduledTasks = new BlockingConcurrentDictionary<ScheduledTask<T>, CancellationTokenSchedulerAction>();
 		}
 
 		/// <summary>
@@ -61,7 +54,7 @@ namespace NerdyDuck.Scheduler
 		/// </summary>
 		private void CancelAllTasks()
 		{
-			foreach (KeyValuePair<ScheduledTask<T>, IAsyncAction> pair in ScheduledTasks)
+			foreach (KeyValuePair<ScheduledTask<T>, CancellationTokenSchedulerAction> pair in ScheduledTasks)
 			{
 				pair.Value.Cancel();
 			}
@@ -83,44 +76,53 @@ namespace NerdyDuck.Scheduler
 						break;
 					if (task.IsTaskDue && !ScheduledTasks.ContainsKey(task))
 					{
-						WorkItemHandler wih = new WorkItemHandler((IAsyncAction operation) =>
-						{
-							AsyncActionSchedulerAction sa = new AsyncActionSchedulerAction(operation);
-							try
-							{
-								if (sa.IsCancellationRequested || OnTaskStarting(task))
-								{
-									sa.IsCanceled = true;
-								}
-								else
-								{
-									task.Run(sa);
-								}
-							}
-							catch (OperationCanceledException ex)
-							{
-								sa.IsSuccess = false;
-								sa.IsCanceled = true;
-								sa.Exception = ex;
-							}
-							catch (Exception ex)
-							{
-								sa.IsSuccess = false;
-								sa.IsCanceled = false;
-								sa.Exception = ex;
-							}
-							finally
-							{
-								OnTaskCompleted(task, sa);
-								SpinWait.SpinUntil(() => { return ScheduledTasks.Remove(task); }, 100);
-								task.UpdateSchedule(DateTimeOffset.Now);
-								task.IsScheduled = false;
-								sa.Operation.Close();
-							}
-						});
+						CancellationTokenSchedulerAction action = new CancellationTokenSchedulerAction();
 						task.IsScheduled = true;
-						ScheduledTasks.Add(task, ThreadPool.RunAsync(wih, WorkItemPriority.Normal, WorkItemOptions.TimeSliced));
+						ScheduledTasks.Add(task, action);
+						ThreadPool.QueueUserWorkItem(new WaitCallback(ThreadPoolCallback), new Tuple<ScheduledTask<T>, CancellationTokenSchedulerAction>(task, action));
 					}
+				}
+			}
+		}
+
+		/// <summary>
+		/// The method that is executed by the thread pool.
+		/// </summary>
+		/// <param name="state">A state object. Contains the task to execute.</param>
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes", Justification = "This is a thread's Main method. No other place to handle any exception.")]
+		private void ThreadPoolCallback(object state)
+		{
+			Tuple<ScheduledTask<T>, CancellationTokenSchedulerAction> Parameters = state as Tuple<ScheduledTask<T>, CancellationTokenSchedulerAction>;
+			try
+			{
+				if (Parameters.Item2.IsCancellationRequested || OnTaskStarting(Parameters.Item1))
+				{
+					Parameters.Item2.IsCanceled = true;
+				}
+				else
+				{
+					Parameters.Item1.Run(Parameters.Item2);
+				}
+			}
+			catch (Exception ex)
+			{
+				Parameters.Item2.IsSuccess = false;
+				Parameters.Item2.IsCanceled = false;
+				Parameters.Item2.Exception = ex;
+			}
+			finally
+			{
+				if (!IsDisposed)
+				{
+					OnTaskCompleted(Parameters.Item1, Parameters.Item2);
+					BlockingConcurrentDictionary<ScheduledTask<T>, CancellationTokenSchedulerAction> tasks = ScheduledTasks;
+					if (tasks != null)
+					{
+						tasks.Remove(Parameters.Item1);
+					}
+					Parameters.Item1.UpdateSchedule(DateTimeOffset.Now);
+					Parameters.Item1.IsScheduled = false;
+					Parameters.Item2.Source.Dispose();
 				}
 			}
 		}
@@ -128,31 +130,27 @@ namespace NerdyDuck.Scheduler
 
 		#region Private classes
 		/// <summary>
-		/// Implementation if <see cref="ISchedulerAction"/> for use with a <see cref="IAsyncAction"/>.
+		/// Implementation if <see cref="ISchedulerAction"/> for use with a <see cref="CancellationTokenSource"/>.
 		/// </summary>
-		internal class AsyncActionSchedulerAction : ISchedulerAction
+		internal class CancellationTokenSchedulerAction : ISchedulerAction, IDisposable
 		{
 			#region Private fields
-			private IAsyncAction mOperation;
+			private CancellationTokenSource mSource;
+			private bool IsDisposed;
 			#endregion
 
-			#region Constructor
-			/// <summary>
-			/// Initializes a new instance of the <see cref="AsyncActionSchedulerAction"/> class with the specified asynchronous operation.
-			/// </summary>
-			public AsyncActionSchedulerAction(IAsyncAction operation)
-			{
-				mOperation = operation;
-			}
-			#endregion
-
+			#region Private fields
 			/// <summary>
 			/// Gets whether cancellation has been requested for this action.
 			/// </summary>
 			/// <value><see langword="true"/>, if the action should be canceled; otherwise, <see langword="false"/>.</value>
 			public bool IsCancellationRequested
 			{
-				get { return mOperation.Status == AsyncStatus.Canceled; }
+				get
+				{
+					AssertDisposed();
+					return mSource.Token.IsCancellationRequested;
+				}
 			}
 
 			/// <summary>
@@ -179,23 +177,44 @@ namespace NerdyDuck.Scheduler
 			public object State { get; set; }
 
 			/// <summary>
-			/// Gets the underlying <see cref="IAsyncAction"/>.
+			/// Gets the underlying <see cref="CancellationTokenSource"/>.
 			/// </summary>
-			public IAsyncAction Operation
+			public CancellationTokenSource Source
 			{
-				get { return mOperation; }
+				get { return mSource; }
 			}
+			#endregion
 
+			#region Constructor
+			/// <summary>
+			/// Initializes a new instance of the <see cref="CancellationTokenSchedulerAction"/> class.
+			/// </summary>
+			public CancellationTokenSchedulerAction()
+			{
+				mSource = new CancellationTokenSource();
+				IsDisposed = false;
+			}
+			#endregion
+
+			#region Destructor
+			/// <summary>
+			/// Destructor.
+			/// </summary>
+			~CancellationTokenSchedulerAction()
+			{
+				Dispose(false);
+			}
+			#endregion
+
+			#region Public methods
 			/// <summary>
 			/// Throws an exception if the action was canceled.
 			/// </summary>
 			/// <exception cref="OperationCanceledException"><see cref="IsCancellationRequested"/> is <see langword="true"/>.</exception>
 			public void ThrowIfCancellationRequested()
 			{
-				if (mOperation.Status == AsyncStatus.Canceled)
-				{
-					throw new OperationCanceledException();
-				}
+				AssertDisposed();
+				mSource.Token.ThrowIfCancellationRequested();
 			}
 
 			/// <summary>
@@ -203,8 +222,52 @@ namespace NerdyDuck.Scheduler
 			/// </summary>
 			public void Cancel()
 			{
-				mOperation.Cancel();
+				AssertDisposed();
+				mSource.Cancel();
 			}
+			#endregion
+
+			#region IDisposable implementation
+			/// <summary>
+			/// Releases allocated resources.
+			/// </summary>
+			public void Dispose()
+			{
+				Dispose(true);
+				GC.SuppressFinalize(this);
+			}
+
+			/// <summary>
+			/// Releases allocated resources.
+			/// </summary>
+			/// <param name="disposing">A value indicating if the method was called by user code. If <see langword="false"/>, the method was called by the runtime in the finalizer.</param>
+			/// <remarks>If <paramref name="disposing"/> is <see langword="false"/>, no other objects should be referenced.</remarks>
+			protected virtual void Dispose(bool disposing)
+			{
+				if (IsDisposed)
+					return;
+
+				IsDisposed = true;
+				if (disposing)
+				{
+					mSource.Dispose();
+				}
+				mSource = null;
+			}
+			#endregion
+
+			#region Private methods
+			/// <summary>
+			/// Checks if the <see cref="ScheduledTask{T}"/> is disposed.
+			/// </summary>
+			private void AssertDisposed()
+			{
+				if (IsDisposed)
+				{
+					throw new ObjectDisposedException(this.ToString());
+				}
+			}
+			#endregion
 		}
 		#endregion
 	}
